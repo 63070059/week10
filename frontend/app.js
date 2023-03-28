28a81f54a8643569c3fcd0593c220d9fe56cd03f
63070059 <nattapat.watt@gmail.com>
2023-03-28 19:08:43 +0700

    add frontend

--- frontend/app.js
+++ frontend/app.js
@@ -4,6 +4,20 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
+const URL="http://3.85.108.108:8088"
+
+app.post("/image", async (req, res) => {
+  console.log(req.body);
+	let image = await fetch(URL)
+	.then((response) => {
+		return response.json()
+	})
+	.catch((error) => {
+		console.log(error);
+	})
+	res.json(image)
+})
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
